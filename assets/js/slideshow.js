380e2a6dd7e4240627f33899f2b46fff06dde5a8
khuan028 <khuan028@ucr.edu>
2018-02-09 11:47:49 -0800

    Home page draft and Oswald font

---
+++ assets/js/slideshow.js
@@ -0,0 +1,10 @@
+$("#slideshow > .slide:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > .slide:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
